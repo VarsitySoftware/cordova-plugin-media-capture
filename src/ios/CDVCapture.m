55f08db4d8f1b1965abac2682792812ceec885ce
Varsity Software <github@varsitysoftware.com>
2017-12-05 02:04:16 -0500

    Update CDVCapture.m

--- src/ios/CDVCapture.m
+++ src/ios/CDVCapture.m
@@ -223,6 +223,7 @@
     // options could contain limit, duration and mode
     // taking more than one video (limit) is only supported if provide own controls via cameraOverlayView property
     NSNumber* duration = [options objectForKey:@"duration"];
+    NSNumber* quality = [options objectForKey:@"quality"];
     NSString* mediaType = nil;
 
     if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
@@ -263,9 +264,20 @@
             //NSLog(@"pickerController.videoMaximumDuration = %f", pickerController.videoMaximumDuration);
         }
 
-        // iOS 4.0
+       // iOS 4.0
         if ([pickerController respondsToSelector:@selector(cameraCaptureMode)]) {
             pickerController.cameraCaptureMode = UIImagePickerControllerCameraCaptureModeVideo;
+            switch((int)([quality doubleValue]*10)) {
+                case 0: 
+                    pickerController.videoQuality = UIImagePickerControllerQualityTypeLow;
+                    break;
+                case 10: 
+                    pickerController.videoQuality = UIImagePickerControllerQualityTypeHigh;
+                    break;
+                default:
+                    pickerController.videoQuality = UIImagePickerControllerQualityTypeMedium;
+                    break;
+            }
             // pickerController.videoQuality = UIImagePickerControllerQualityTypeHigh;
             // pickerController.cameraDevice = UIImagePickerControllerCameraDeviceRear;
             // pickerController.cameraFlashMode = UIImagePickerControllerCameraFlashModeAuto;
